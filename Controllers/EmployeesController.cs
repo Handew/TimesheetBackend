0c3ed4633d99146f2083536a772982f7ea3738f0
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-04-15 11:05:46 +0300

    eka commit

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimesheetBackend.Models;
+
+namespace TimesheetBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private tuntidbContext db = new tuntidbContext();
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult GetAllActiveEmployees()
+        {
+            try
+            {
+                var emp = (from e in db.Employees where e.Active == true select e).ToList();
+                return Ok(emp);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Virhe: " + e);
+            }
+            finally
+            {
+                db.Dispose();
+            }
+        }
+
+
+
+    }
+}
---
+++ Controllers/WorkAssignmentsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimesheetBackend.Models;
+
+namespace TimesheetBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkAssignmentsController : ControllerBase
+    {
+        private tuntidbContext db = new tuntidbContext();
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult GetAllActiveWorkAssignments()
+        {
+            try
+            {
+                var wa = (from w in db.WorkAssignments where w.Active == true select w).ToList();
+                return Ok(wa);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Virhe: " + e);
+            }
+            finally
+            {
+                db.Dispose();
+            }
+        }
+    }
+}
